4efdc2300048312f2530b6d85b9e5982507144f0
GoForWalk <atcgsp912@gmail.com>
2020-06-24 15:47:37 +0900

    .

--- src/main/java/com/sm/resttemplate/WeatherAPI.java
+++ src/main/java/com/sm/resttemplate/WeatherAPI.java
@@ -1,30 +1,50 @@
 package com.sm.resttemplate;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.util.HashMap;
 
+import org.hibernate.mapping.Map;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 public class WeatherAPI {
 	
-	private static final String WEATHER_API_KEY
-		= "2XvzikuY6DfCNryEkAgrB8IeC8g6W%2BIuc4zqEuKB24Z2Pd3%2B2KDw1JuiUj%2FByKxcZM8vsFFEvYkOfegKRvpADw%3D%3D";
+	@Value("${spring.weather.key}")
+	private String WEATHER_API_KEY;
 
-	@GetMapping("/GetWeatherData")
-	public String callAPI() {
+	@GetMapping("/getWeatherData")
+	public String callWeatherAPI() {
 		
-		StringBuffer result = new StringBuffer();
+		HashMap<String, Object> result = new HashMap<String, Object>();
+		
+		String jsonInString = "";
+		
 		
 		try {
+			
+			HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+			factory.setConnectTimeout(5000);
+			factory.setReadTimeout(5000);
+			RestTemplate restTemplate = new RestTemplate(factory);
+			
+			HttpHeaders header = new HttpHeaders();
+			HttpEntity<?>entity = new HttpEntity<>(header);
 			
 			String urlStr = "http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst?"
 					+ "serviceKey" + WEATHER_API_KEY
 					+ "pageNo" + "1" 
-					+ "numOfRows" + "9"
+					+ "numOfRows" + "255"
 					+ "dataType" + "JSON"
 					+ "base_date" + "20200622"
 					+ "base_time" + "0500"
@@ -32,24 +52,32 @@ public class WeatherAPI {
 					+ "ny" + "127"
 					;
 			
-			URL url = new URL(urlStr);
-			HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-			urlConnection.setRequestMethod("GET");
+//			UriComponents uri = UriComponentsBuilder.fromHttpUrl(urlStr);
 			
-			BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
+			// API 호출하며 MAP 타입으로 전달 받는다.
+			ResponseEntity<Map>resultMap = restTemplate.exchange(urlStr, HttpMethod.GET, entity, Map.class);
 			
-			String returnLine;
-			result.append("<xmp>");
-			while((returnLine = br.readLine()) != null) {
-				result.append(returnLine + "\n");
-			}
+			result.put("statusCode", resultMap.getStatusCodeValue()); // statusCode 확인
+			result.put("header", resultMap.getHeaders()); // 헤더정보
+			result.put("body", resultMap.getBody()); // 실제 데이터
 			
-			urlConnection.disconnect();
+			ObjectMapper mapper = new ObjectMapper();
+			jsonInString = mapper.writeValueAsString(resultMap.getBody());
+			
+			
+		} catch (HttpClientErrorException | HttpServerErrorException e) {
+			
+			result.put("statusCode", e.getRawStatusCode());
+			result.put("body" ,e.getStatusText());
+			System.out.println(e.toString());
+			
 		} catch (Exception e) {
-			e.printStackTrace();
-		}
+			result.put("statusCode", 999);
+			result.put("body", "exception error");
+			System.out.println(e.toString());
 		
-		return result + "</xmp>";
+		}
+		return jsonInString;
 	}
 	
 }
