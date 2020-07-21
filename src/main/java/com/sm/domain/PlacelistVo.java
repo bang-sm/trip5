6cd94d8d634bb1e3f781463223d67a7de97b5bee
gksfk6165 <gksfk308@gmail.com>
2020-07-21 09:13:18 +0900

    2020-07-21
    visit & chart 화면 : 차트 표현, 방문했던곳 표현,
    
    //TODO 지도에 현황표시하기

--- src/main/java/com/sm/domain/PlacelistVo.java
+++ src/main/java/com/sm/domain/PlacelistVo.java
@@ -40,4 +40,39 @@ public class PlacelistVo {
 	private Date placeregdate;
 	private int placecheck;
 	private int bookmark;
+	
+	//donut-chart 음식 개수
+	private int foodcount;
+	//donut-chart 카페 개수
+	private int cafecount;
+	//donut-chart 관광지 개수
+	private int placecount;
+	
+	// bar-chart 계획한 음식 개수
+	private int planfood;
+	// bar-chart 계획한 카페 개수
+	private int plancafe;
+	// bar-chart 계획한 관광지 개수
+	private int planplace;
+	
+	// bar-chart 방문한 음식 개수
+	private int visitfood;
+	// bar-chart 방문한 카페 개수
+	private int visitcafe;
+	// bar-chart 방문한 관광지 개수
+	private int visitplace;
+	
+	//linechart 몇월부터
+	private String firstdate;
+	//linechart 몇월 까지
+	private String enddate;
+	
+	private String foodtitle;
+	private String cafetitle;
+	private String placetitle;
+	
+	
+	
+	
+	
 }
