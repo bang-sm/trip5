e517d0c8afd3c147f19d026ecba16b0dbc513f60
bang-sm <tjrals1991@gmail.com>
2020-07-21 19:08:05 +0900

    2020-07-21
    각 day별 텍스트에디터(summernote) 데이터베이스에서 읽어와 로딩시키기 완료
    경로 삭제 위치 변경 업데이트 상황에 맞춰 name index 정렬
    임시 저장시 key값 체크하여 있을시 update 없을시 insert 확인
    
    TOOD // 화면에 표출하여 뿌리기 , 경로 데이터를 이용하여 이미지 표출하기 , 사진첨부하기 , 일지 프로그레스바 클릭시 해당
    일지 form으로 이동하기
    
    by 방석민

--- src/main/java/com/sm/controller/TravelController.java
+++ src/main/java/com/sm/controller/TravelController.java
@@ -1,27 +1,33 @@
 package com.sm.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.sm.domain.TravelInfoRootVO;
+import com.sm.domain.TravelInfoVO;
 import com.sm.domain.TravelVO;
 import com.sm.service.TravelService;
 
 /**
- * @author smbang 작업 컨트롤러
- * 일지등록 관련한 컨트롤러입니다.
+ * @author smbang 작업 컨트롤러 일지등록 관련한 컨트롤러입니다.
  */
 @Controller
 @RequestMapping(value = "/travel")
 public class TravelController {
 	private static final Logger logger = LoggerFactory.getLogger(TravelController.class);
-	
+
 	@Autowired
 	private TravelService travelService;
 
@@ -45,42 +51,78 @@ public class TravelController {
 
 		return "/travel/intro_date";
 	}
-	
+
 	/**
-	 * @author smbang 첫 등록화면에서 날짜와 제목 선택후 저장 시킬 맵핑
-	 * 제목과 날짜 우선으로 일지를 우선저장 합니다.
+	 * @author smbang 첫 등록화면에서 날짜와 제목 선택후 저장 시킬 맵핑 제목과 날짜 우선으로 일지를 우선저장 합니다.
 	 */
 	@ResponseBody
 	@PostMapping("/travel_firstSave")
-	public TravelVO travel_firstSave(TravelVO travelVO) {
+	public void travel_firstSave(@ModelAttribute TravelVO travelVO) {
 		logger.info("travel_firstSave");
 		travelService.travel_firstSave(travelVO);
-		return travelVO;
 	}
 	
+	/**
+	 * @author smbang 일지디테일 임시저장용 맵핑
+	 */
+	@ResponseBody
+	@PostMapping("/travel_temp_save")
+	public String travel_temp_save(@ModelAttribute TravelInfoVO travelInfoVO,
+			@ModelAttribute TravelInfoRootVO travelInfoRootVO) {
+		logger.info("travel_temp_save");
+		
+		travelService.tempSave(travelInfoVO,travelInfoRootVO);
+		
+		return "다녀왔습니다";
+	}
+	@ResponseBody
+	@PostMapping(value = "/travel_root_delete")
+	public String travel_root_delete(int tsirootorder,int tsid) throws Exception {
+		logger.info("travel_root_delete");
+		
+		System.out.println(tsirootorder + " /  "+tsid);
+		travelService.travel_root_delete(tsirootorder,tsid);
+
+		return "삭제되었습니다";
+	}
+	@ResponseBody
+	@PostMapping(value = "/travel_detail_data")
+	public List<TravelInfoVO> travel_detail_data(String tsid) throws Exception {
+		logger.info("travel_detail_data");
+		return travelService.getTravelInfo(tsid);
+	}
+
 	/**
 	 * @author smbang 일지 디테일 등록할 화면 맵핑
 	 */
 	@GetMapping(value = "/regist")
-	public String regist_get(Model model,String tsid) {
+	public String regist_get(Model model, String tsid) {
 		logger.info("regist_get");
-		
-		tsid="79";
-		
-		//일지 id값없이 접근하면 홈으로 돌려보낸다.
-		if(tsid=="" || tsid==null) {
+
+		tsid = "84";
+
+		// 일지 id값없이 접근하면 홈으로 돌려보낸다.
+		if (tsid == "" || tsid == null) {
 			return "redirect:/index";
 		}
-		model.addAttribute("travel",travelService.getTravelStory(tsid));
-		System.out.println(travelService.getTravelStory(tsid));
+		model.addAttribute("travel", travelService.getTravelStory(tsid)); //기본정보
+		model.addAttribute("travelInfo", travelService.getTravelInfo(tsid)); //임시저장된 상세정보
+		model.addAttribute("travelRootInfo", travelService.getTravelRootInfo(tsid)); //임시저장된 루트정보
 		return "/travel/regist";
 	}
-	/*
-	 * @PostMapping(value = "/regist") public String regist_get(TravelVO
-	 * travelVO,@ModelAttribute TravelInfoVO travelinfoVO,HttpSession session)
-	 * throws Exception{ logger.info("regist_get");
-	 * travelService.storyRegist(travelVO,travelinfoVO);
-	 * 
-	 * return "redirect:/index"; }
-	 */
+	@PostMapping(value = "/registSave")
+	public String regist_get(TravelVO travelVO, @ModelAttribute TravelInfoVO travelinfoVO,
+			@ModelAttribute TravelInfoRootVO travelinfoRootVO, HttpSession session) throws Exception {
+		logger.info("regist_get");
+		// travelService.storyRegist(travelVO,travelinfoVO);
+
+		System.out.println(travelinfoVO);
+		System.out.println(travelinfoRootVO.getRootlist().get(0).getTsirootname());
+		System.out.println(travelinfoRootVO.getRootlist().get(1).getTsirootname());
+
+		return "redirect:/index";
+	}
+	
+
+
 }
--- src/main/java/com/sm/service/TravelService.java
+++ src/main/java/com/sm/service/TravelService.java
@@ -1,12 +1,15 @@
 package com.sm.service;
 
+import java.util.HashMap;
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sm.dao.TravelDAO;
-import com.sm.domain.MemberVO;
+import com.sm.domain.TravelInfoRootVO;
 import com.sm.domain.TravelInfoVO;
 import com.sm.domain.TravelVO;
 
@@ -28,12 +31,12 @@ public class TravelService {
 
 	/**
 	 * @author smbang 일지의 기본 데이터 저장 하기 위한
-	 *  매소드 제목 , 시작날짜 , 마지막날짜
+	 *  매소드 제목 , 시작날짜 , 마지막날짜 , 기간 만큼 미리  DAY별 임시저장하고 키를 미리 받아 리턴시킨다. 
 	 */
 	public void travel_firstSave(TravelVO travelVO) {
-		MemberVO memberVO = new MemberVO();
-		memberVO = (MemberVO) session.getAttribute("userInfo");
-		travelVO.setUuid(memberVO.getUuid());
+		//MemberVO memberVO = new MemberVO();
+		//memberVO = (MemberVO) session.getAttribute("userInfo");
+		//travelVO.setUuid(memberVO.getUuid());
 		
 		travelDAO.travel_firstSave(travelVO);
 	}
@@ -46,7 +49,39 @@ public class TravelService {
 		//MemberVO memberVO = new MemberVO();
 		//memberVO = (MemberVO) session.getAttribute("userInfo");
 		
-		return travelDAO.getTravelStory(tsid,9);
+		return travelDAO.getTravelStory(tsid,7);
+	}
+	
+	/**
+	 * @author smbang 
+	 * 일지의 디테일 getTravelInfo 기본 정보를 받아오기위해서
+	 * 일지의 디테일 TravelInfoRootVO 루트정보를 받아오기위해서
+	 */
+	public List<TravelInfoVO> getTravelInfo(String tsid) {
+		return travelDAO.getTravelInfo(tsid);
+	}
+	public List<TravelInfoRootVO> getTravelRootInfo(String tsid) {
+		// TODO Auto-generated method stub
+		return travelDAO.getTravelRootInfo(tsid);
+	}
+
+	/**
+	 * @author smbang 
+	 * 일지 임시저장 데이터를 저장합니다.
+	 */
+	public void tempSave(TravelInfoVO travelInfoVO, TravelInfoRootVO travelInfoRootVO) {
+		travelDAO.tempSave(travelInfoVO,travelInfoRootVO);
+	}
+	
+	/**
+	 * @author smbang 
+	 * 경로삭제
+	 */
+	public void travel_root_delete(int tsirootorder, int tsid) {
+		HashMap<String, Integer> map=new HashMap<String, Integer>();
+		map.put("tsirootorder", tsirootorder);
+		map.put("tsid", tsid);
+		travelDAO.travel_root_delete(map);
 	}
 
 }
