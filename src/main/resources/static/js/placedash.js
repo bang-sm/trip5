99dead5e6a6465cb3caee6fb4f34be39a5005b2e
gksfk6165 <gksfk308@gmail.com>
2020-08-02 18:13:45 +0900

    sidebar 반응형 불러오는 버튼 구현

--- src/main/resources/static/js/placedash.js
+++ src/main/resources/static/js/placedash.js
@@ -9,3 +9,33 @@ $(window).resize(function(){
 		$('body').addClass('sidebar-collapse');
 	}
 }).resize();
+
+$('.pushmenu').click(function(){
+	console.log("click");	
+	$('.main-sidebar').toggleClass('.button-sidebar');
+	if($('.main-sidebar').hasClass('.button-sidebar')==true){
+		$('.main-sidebar').css("margin","0px");
+		$('.side-pushmenu').css("display","block");
+		$('.pushmenu').css("display","none");
+	}else{
+		$('.main-sidebar').css("margin","");
+		$('.side-pushmenu').css("display","none");
+		$('.pushmenu').css("display","block");
+	}
+
+})
+$('.side-pushmenu').click(function(){
+	console.log("click");	
+	$('.main-sidebar').toggleClass('.button-sidebar');
+	if($('.main-sidebar').hasClass('.button-sidebar')==true){
+		$('.main-sidebar').css("margin","0px");
+		$('.side-pushmenu').css("display","block");
+		$('.pushmenu').css("display","none");
+	}else{
+		$('.main-sidebar').css("margin","");
+		$('.side-pushmenu').css("display","none");
+		$('.pushmenu').css("display","block");
+		
+	}
+	
+})
