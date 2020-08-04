82ff703c34715cb3d557da13ba9db3d534fc2539
bang-sm <tjrals1991@gmail.com>
2020-08-04 16:59:26 +0900

    2020-08-04
    대표이미지~

--- src/main/java/com/sm/Utils/FileUtils.java
+++ src/main/java/com/sm/Utils/FileUtils.java
@@ -39,7 +39,12 @@ public class FileUtils {
         if(!target.exists()) target.mkdirs();
         
         for(int i=0; i<file.length; i++) {
-       
+        	String photoMain="";
+        	if(i==0) {
+        		photoMain="Y";
+        	}else {
+        		photoMain="N";
+        	}
             String orgFileName = file[i].getOriginalFilename();
             String orgFileExtension="";
             if(orgFileName.lastIndexOf(".")==-1) {
@@ -60,6 +65,7 @@ public class FileUtils {
             
             Map<String, Object> fileInfo = new HashMap<String, Object>();
             
+            fileInfo.put("photoMain", photoMain);
             fileInfo.put("photo_path", orgFileName);
             fileInfo.put("ts_id", ts_id);
             fileInfo.put("photo_original_name", orgFileName);
